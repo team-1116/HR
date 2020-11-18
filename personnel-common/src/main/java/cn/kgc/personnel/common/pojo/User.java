4518be959ef8984b0ce7bbb37a6494861c7c6859
城南花已开 <330795547@qq.com>
2020-11-18 10:03:09 +0800

    添加基本表信息

---
+++ personnel-common/src/main/java/cn/kgc/personnel/common/pojo/User.java
@@ -0,0 +1,67 @@
+package cn.kgc.personnel.common.pojo;
+
+/**
+ * @program: personnel
+ * @ClassName User
+ * @description:
+ * @author: 熊盛涛
+ * @create: 2020-11-18 09:58
+ * @Version 1.0
+ **/
+/*
+    用户表
+ */
+public class User {
+    /*
+        用户主键id
+     */
+    private Integer UserId;
+
+    /*
+        用户登录名
+     */
+    private String userName;
+
+    /*
+        用户登录密码
+     */
+
+    private String password;
+
+    /*
+        关联员工表id
+     */
+    private Integer emplpyeesId;
+
+    public Integer getUserId() {
+        return UserId;
+    }
+
+    public void setUserId(Integer userId) {
+        UserId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getEmplpyeesId() {
+        return emplpyeesId;
+    }
+
+    public void setEmplpyeesId(Integer emplpyeesId) {
+        this.emplpyeesId = emplpyeesId;
+    }
+}
